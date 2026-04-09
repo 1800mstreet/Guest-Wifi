2736fa40f9a318ea0ce2e313fc924dfa79d1a3f8
1800mstreet <cryptocoin8463@gmail.com>
2026-04-09 17:41:05 -0400

    splash.js

--- splash.js
+++ splash.js
@@ -2,11 +2,25 @@ function connect(event) {
   if (event) event.preventDefault();
 
   const agree = document.getElementById('agree');
-  if (!agree || !agree.checked) {
+
+  if (!agree.checked) {
     alert('Please agree to the Terms and Conditions.');
     return false;
   }
 
-  window.location.href = 'http://neverssl.com';
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const baseGrantUrl = urlParams.get("base_grant_url");
+  const userContinueUrl = urlParams.get("user_continue_url") || "http://neverssl.com";
+
+  if (!baseGrantUrl) {
+    alert("Error: Missing Meraki grant URL");
+    return false;
+  }
+
+  const finalUrl = baseGrantUrl + "?continue_url=" + encodeURIComponent(userContinueUrl);
+
+  window.location.href = finalUrl;
+
   return false;
 }
